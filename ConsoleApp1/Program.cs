53ee375067f9aaf9958ae7be76d2896b9bf05652
Krejcicek <94050047+Krejcicek@users.noreply.github.com>
2024-10-31 12:19:40 +0100

    skoro hotovo pridane zvirata atd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,19 +1,20 @@
-class Zvire
+
+public class Zvire
 {
     private string zvuk;
     private string názevZ;
     private string rasa;
 
-    public Zvire(string zvuk, string rasa, string názevZ)
+    public Zvire(string zvuk, string názevZ, string rasa)
     {
         this.zvuk = zvuk;
-        this.rasa = rasa;
         this.názevZ = názevZ;
+        this.rasa = rasa;
     }
 
-    public string GetZvuk()
+    public void VydatZvuk()
     {
-        return zvuk;
+        Console.WriteLine(zvuk);
     }
 
     public string GetNazevZ()
@@ -25,20 +26,151 @@ class Zvire
     {
         return rasa;
     }
+}
 
-    public void SetZvuk(string novyZvuk)
+public class Pes : Zvire
+{
+    public Pes(string rasa) : base("Haf haf", "Pes", rasa) { }
+}
+
+public class Kocka : Zvire
+{
+    public Kocka(string rasa) : base("Mňau mňau", "Kočka", rasa) { }
+}
+
+public class Ptak : Zvire
+{
+    public Ptak(string rasa) : base("Píp píp", "Pták", rasa) { }
+}
+
+public class Had : Zvire
+{
+    public Had(string rasa) : base("Ssssssss", "Had", rasa) { }
+}
+
+public class Kralik : Zvire
+{
+    public Kralik(string rasa) : base("Vrrrrr (potichu)", "Králík", rasa) { }
+}
+
+
+
+
+
+public class Kun : Zvire
+{
+    public Kun(string rasa) : base("Ihahaha", "Kůň", rasa) { }
+}
+public class Osel : Zvire
+{
+    public Osel(string rasa) : base("Ia ia", "Osel", rasa) { }
+}
+public class Uzovka : Zvire
+{
+    public Uzovka(string rasa) : base("Ssssssss", "Užovka", rasa) { }
+}
+
+
+
+
+
+public class Zmije : Zvire
+{
+    public Zmije(string rasa) : base("Sssssssssss", "Zmije", rasa) { }
+}
+public class Kapr : Zvire
+{
+    public Kapr(string rasa) : base("p p", "Kapr", rasa) { }
+}
+
+public class Papousek : Zvire
+{
+    public Papousek(string rasa) : base("Jak se máš?", "Papoušek", rasa) { }
+}
+
+
+
+public static class VytvorZvire
+{
+
+    public static Zvire VytvorPsa(string rasa)
     {
-        zvuk = novyZvuk;
+        return new Pes(rasa);
     }
 
-    public void SetNazevZ(string novyNazev)
+    public static Zvire VytvorKocku(string rasa)
     {
-        názevZ = novyNazev;
+        return new Kocka(rasa);
     }
 
-    public void SetRasa(string novaRasa)
+    public static Zvire VytvorPtaka(string rasa)
     {
-        rasa = novaRasa;
+        return new Pták(rasa);
     }
+
+
+
+
+
+
+   
+    public static Zvire VytvorHada(string rasa)
+    {
+        return new Had(rasa);
+    }
+
+   
+    public static Zvire VytvorKralika(string rasa)
+    {
+        return new Kralik(rasa);
+    }
+
     
+    public static Zvire VytvorKone(string rasa)
+    {
+        return new Kun(rasa);
+    }
+
+
+
+
+
+
+  
+    public static Zvire VytvorOsla(string rasa)
+    {
+        return new Osel(rasa);
+    }
+
+    /
+    public static Zvire VytvorUzovku(string rasa)
+    {
+        return new Uzovka(rasa);
+    }
+
+   
+    public static Zvire VytvorZmiji(string rasa)
+    {
+        return new Zmije(rasa);
+    }
+
+
+
+
+
+
+    public static Zvire VytvorKapra(string rasa)
+    {
+        return new Kapr(rasa);
+    }
+
+    public static Zvire VytvorPapouska(string rasa)
+    {
+        return new Papousek(rasa);
+    }
+
+
+
+
+
 }
